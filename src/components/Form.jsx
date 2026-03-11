1aeecf7a913ec4b9027e26b10ba0e66937d981e3
Sumit dhakal <sumitdhakal082@gmail.com>
2026-03-11 10:43:31 +0545

    feat remove unnecessary code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,87 +1,104 @@
 import React from "react";
+import FormField from "./ui/FormField";
+const Gender = [
+  { value: "male", label: "Male" },
+  { value: "female", label: "Female" },
+  { value: "other", label: "Other" },
+];
+
+const Education = [
+  { value: "highSchool", label: "High School" },
+  { value: "higherSecondary", label: "Higher Secondary" },
+  { value: "diploma", label: "Diploma" },
+  { value: "bachelor", label: "Bachelor Degree" },
+  { value: "master", label: "Master Degree" },
+  { value: "phd", label: "PhD" },
+];
+
+const Experience = [
+  { value: "fresher", label: "Fresher" },
+  { value: "1-2", label: "1-2 years" },
+  { value: "2-3", label: "2-3 years" },
+  { value: "5+", label: "5+ years" },
+];
+
+const JobType = [
+  {
+    value: "fulltime",
+    label: "Full-time",
+  },
+  { value: "internship", label: "Internship" },
+  { value: "contract", label: "Contract" },
+];
+
+const WorkMode = [
+  { vale: "remote", label: "Remote" },
+  { vale: "hybrid", label: "Hybrid" },
+  { vale: "onSite", label: "On-Site" },
+];
 
 const Form = () => {
   return (
     <form
       action=""
-      className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl mx-auto"
+      className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-full mx-auto"
     >
       <div className="flex flex-col gap-4">
-        <div>
-          <label className="uppercase">age</label>
-          <input name="age" type="number" placeholder="e.g. 25" />
-        </div>
-        <div>
-          <label className="uppercase">Gender</label>
-          <select name="" id="">
-            <option value="" disabled selected>
-              Select Gender
-            </option>
-            <option value="male">Male</option>
-            <option value="">Female</option>
-            <option value="">Other</option>
-          </select>
-        </div>
-        <div>
-          <label className="uppercase">Highest Education</label>
-          <select name="" id="">
-            <option value="" disabled selected>
-              Select Education
-            </option>
-            <option value="highSchool">High School</option>
-            <option value="higherSecondary">Higher Secondary</option>
-            <option value="diploma">Diploma</option>
-            <option value="bachelor">Bachelor's Degree</option>
-            <option value="master">Master's Degree</option>
-            <option value="phd">PhD</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
-        <div>
-          <label className="capitalize">field of study</label>
-          <input type="text" placeholder="e.g. Computer Science" />
+        <div className=" flex flex-col gap-4">
+          <FormField
+            label="Age"
+            name="age"
+            type="number"
+            placeholder="e.g. 25"
+          />
+          <FormField
+            label="Skills"
+            name="skills"
+            type="text"
+            placeholder="Enter your skills"
+          />
+          <FormField
+            label="Highest Education"
+            name="education"
+            options={Education}
+            placeholder="Select Education"
+          />
+          <FormField
+            label="field of study"
+            name="study"
+            type="text"
+            placeholder="e.g. Computer Science"
+          />
         </div>
       </div>
       {/* second */}
       <div className="flex flex-col gap-4">
-        <div>
-          <label className="capitalize">Skills</label>
-          <input type="text" placeholder="Enter your skills" />
-        </div>
-        <div>
-          <label htmlFor="">Experience Level</label>
-          <select name="" id="">
-            <option value="" disabled selected>
-              Select Experience
-            </option>
-            <option value="fresher">Fresher</option>
-            <option value="1-2">1-2 years</option>
-            <option value="2-3">2-3 years</option>
-            <option value="5+">5+ years</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="">Job Type</label>
-          <select name="" id="">
-            <option value="" disabled selected>
-              Select job type
-            </option>
-            <option value="fulltime">Full-time</option>
-            <option value="parttime">Part-time</option>
-            <option value="internship">Internship</option>
-            <option value="contract">Contract</option>
-          </select>
-        </div>
-        <div>
-          <label className="uppercase">Work Mode</label>
-          <select>
-            <option value="" disabled selected>
-              Select Work Mode
-            </option>
-            <option value="remote">Remote</option>
-            <option value="hybrid">Hybrid</option>
-            <option value="onsite">On-site</option>
-          </select>
+        <div className=" flex flex-col gap-4">
+          <FormField
+            label="Gender"
+            name="gender"
+            options={Gender}
+            placeholder="Select Gender"
+          />
+          <FormField
+            label="Experience Level"
+            name="experience"
+            options={Experience}
+            placeholder="Select Experience"
+          />
+          <FormField
+            label="Job Type"
+            name="job"
+            options={JobType}
+            placeholder="Select job type"
+          />
+
+          <FormField
+            label="Work Mode"
+            name="workMode"
+            options={WorkMode}
+            placeholder=" Select Work Mode"
+          />
         </div>
       </div>
       <div className=" md:col-span-2 flex justify-center border bg-blue-600 text-white p-4 rounded-2xl">
