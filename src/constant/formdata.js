d6f6f4de38affe4dad54643114b0d1de8e00808d
Sumit dhakal <sumitdhakal082@gmail.com>
2026-03-11 10:46:45 +0545

    feat create sperate file for constant

---
+++ src/constant/formdata.js
@@ -0,0 +1,36 @@
+export const Gender = [
+  { value: "male", label: "Male" },
+  { value: "female", label: "Female" },
+  { value: "other", label: "Other" },
+];
+
+export const Education = [
+  { value: "highSchool", label: "High School" },
+  { value: "higherSecondary", label: "Higher Secondary" },
+  { value: "diploma", label: "Diploma" },
+  { value: "bachelor", label: "Bachelor Degree" },
+  { value: "master", label: "Master Degree" },
+  { value: "phd", label: "PhD" },
+];
+
+export const Experience = [
+  { value: "fresher", label: "Fresher" },
+  { value: "1-2", label: "1-2 years" },
+  { value: "2-3", label: "2-3 years" },
+  { value: "5+", label: "5+ years" },
+];
+
+export const JobType = [
+  {
+    value: "fulltime",
+    label: "Full-time",
+  },
+  { value: "internship", label: "Internship" },
+  { value: "contract", label: "Contract" },
+];
+
+export const WorkMode = [
+  { vale: "remote", label: "Remote" },
+  { vale: "hybrid", label: "Hybrid" },
+  { vale: "onSite", label: "On-Site" },
+];
