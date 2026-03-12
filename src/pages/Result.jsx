eeb80a1c23eb8d9a53076ffca5f54df94422d0c6
Sumit dhakal <sumitdhakal082@gmail.com>
2026-03-12 08:35:19 +0545

    feat add api to search and fech loksewa vaceny from psc website

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,10 +1,42 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 const Result = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const data = location.state;
+
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchjobs = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/jobs");
+        const result = await res.json();
+        let filtered = result.jobs;
+        setJobs(filtered);
+      } catch (error) {
+        setError("Failed to fetch jobs. Make sure backend is running.", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchjobs();
+  }, []);
   return (
     <section>
       <div>
         <button> ← Back to Search</button>
+        <div>
+          <h2>loksewa vaceny found for you</h2>
+          <span>based on the notice from PSC website</span>
+        </div>
+        <div>
+          {jobs.map((job) => (
+            <span key={job.id}>{job.title}</span>
+          ))}
+        </div>
       </div>
     </section>
   );
